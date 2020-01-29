149fc82f5165df803ec18608f15637126a0a2aa4
Dennis Bartlett <bartlett.dc.1@gmail.com>
2020-01-28 23:51:08 -0500

    Work's Done

--- entities/Creature.ts
+++ entities/Creature.ts
@@ -2,10 +2,10 @@ import { Attacks } from "./Attacks";
 import { Types } from "./Types";
 
 export default interface Creature {
-  "name": String,
-  "health": Number,
-  "attack": Number,
-  "defence": Number,
-  "types": Types,
-  "attacks": Attacks
+  name: String,
+  health: Number,
+  attack: Number,
+  defence: Number,
+  types: Types,
+  attacks: Attacks
 }
